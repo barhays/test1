02e9c271a46606ab6543aae93381460c053cd160
vlad <lukyanovvlad2005@gmail.com>
2023-10-31 15:21:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const question1={
+const question1 ={
     numberOfQuestion: 1,
     textOfQuestion: "How many planets are in the solar system?",
     answers: [8,9,10,12],
     rightanswer: [8]
 }
-const question2={
+const question2 ={
     numberOfQuestion: 2,
     textOfQuestion: "What is the freezing point of water?",
     answers: [0,-5,-6,1],
     rightanswer: [0]
 }
-const question3={
+const question3 ={
     numberOfQuestion: 3,
     textOfQuestion: "What is the longest river in the world?",
     answers: ["Nile", "Amazon", "Yangtze"],
     rightanswer: ["Nile"]
     
 }
-const question4={
+const question4 ={
     numberOfQuestion: 4,
     textOfQuestion: "How many chromosomes are in the human genome?",
     answers: [42, 44, 46, 58, 72],
     rightanswer: [46]
 }
-const question5={
+const question5 ={
     numberOfQuestion: 5,
     textOfQuestion: "Which of these characters are friends with Harry Potter?",
     answers: ["Ron Weasley", "Draco Malfoy", "Hermione Granger"],
     rightanswer: ["Ron Weasley", "Hermione Granger"]
 }
-const question6={
+const question6 ={
     numberOfQuestion: 6,
     textOfQuestion: "What is the capital of Canada?",
     answers: ["Toronto", "Ottawa", "Vancouver"],
     rightanswer: ["Ottawa"]
 }
-const question7={
+const question7 ={
     numberOfQuestion: 7,
     textOfQuestion: "What is the Jewish New Year called?",
     answers: ["Hanukkah", "Yom Kippur", "Kwanzaa", "Rosh Hashanah"],
@@ -44,9 +44,12 @@ const question7={
 
 let Questions = [question1 , question2 , question3 , question4 , question5 , question6 , question7]
 
-let counter=0;
+let counter = 0;
 
-let answersChecked=false;
+let answersChecked = false;
+
+let rightanswercounter = 0;
+
 
 function FormCreate(Questions){
     let title = document.querySelector(".title");
@@ -57,11 +60,11 @@ function FormCreate(Questions){
     let questionText = document.createElement('h3');
     questionText.innerText = Questions[counter].textOfQuestion;
     question.appendChild(questionText);
-    for(let i=0; i<Questions[counter].answers.length; i++)
+    for(let i = 0; i < Questions[counter].answers.length; i++)
     {
-        let buttons=document.querySelector(".buttons");
-        let button=document.createElement('button');
-        button.innerText=Questions[counter].answers[i];
+        let buttons = document.querySelector(".buttons");
+        let button = document.createElement('button');
+        button.innerText = Questions[counter].answers[i];
         buttons.appendChild(button);
     }
     if(Questions[counter].rightanswer.length>1)
@@ -71,13 +74,21 @@ function FormCreate(Questions){
         moreInfotext.innerText = "This question have more then one answer";
         moreInfo.appendChild(moreInfotext);
     }
+    let nextButton = document.querySelector(".nextButton");
+    let button = document.createElement('button');
+    button.innerText = "Next";
+    nextButton.appendChild(button);
 }
 
 function DeleteItems(){
     let deleteTitle = document.querySelector(".title")
-    let deleteQuestion=document.querySelector(".question")
-    let deleteButtons=document.querySelector(".buttons")
-    let deleteMoreInfo=document.querySelector(".moreInfo")
+    let deleteQuestion = document.querySelector(".question")
+    let deleteButtons = document.querySelector(".buttons")
+    let deleteMoreInfo = document.querySelector(".moreInfo")
+    let deleteNextButton = document.querySelector(".nextButton")
+    let deleteAgainButton = document.querySelector(".againButton")
+    deleteAgainButton.innerHTML = '';
+    deleteNextButton.innerHTML = '';
     deleteTitle.innerHTML = '';
     deleteQuestion.innerHTML = '';
     deleteButtons.innerHTML = '';
@@ -91,7 +102,7 @@ function FinishWindow(){
     title.appendChild(titleText);
     let moreInfo = document.querySelector(".moreInfo");
     let MoreInfoText = document.createElement('h3');
-    MoreInfoText.innerText = "Number of correct answers:";
+    MoreInfoText.innerText = "Number of correct answers:"+ rightanswercounter;
     moreInfo.appendChild(MoreInfoText);
     let againButton = document.querySelector(".againButton");
     let button = document.createElement('button');
@@ -99,48 +110,63 @@ function FinishWindow(){
     againButton.appendChild(button);
 }
 
-function NextButtonCreate(){
-    let nextButton = document.querySelector(".nextButton");
-    let button = document.createElement('button');
-    button.innerText = "Next";
-    nextButton.appendChild(button);
-}
-
 onCreate();
 
 function onCreate()
 {
     FormCreate(Questions)
-    NextButtonCreate()
-    
-    if(answersChecked==true)
-    {
-        let nextbttn=document.querySelector(".nextButton");
-        nextbttn.querySelector("button").addEventListener('click',()=>{
-            counter++;
-            if(counter <= Questions.length-1)
+    document.querySelectorAll(".buttons").forEach(elem => {
+        elem.addEventListener('click', (e)=>{
+            if(answersChecked == false)
             {
-                DeleteItems();
-                FormCreate(Questions);
-            }
-            else
-            {
-                let deleteNextButton=document.querySelector(".nextButton")
-                deleteNextButton.innerHTML = '';
-                DeleteItems();
-                FinishWindow();  
-                let againbttn=document.querySelector(".againButton");
-                againbttn.querySelector("button").addEventListener('click',() =>{
-                    
-                    counter=0;
-                    DeleteItems();
-                    let deleteAgainButton=document.querySelector(".againButton")
-                    deleteAgainButton.innerHTML = '';
-                    onCreate();
-            })
-            }
+                console.log("im here")
+                if(Questions[counter].rightanswer.length == 1)
+                {
+                    if(Questions[counter].rightanswer[0] == e.target.innerText)
+                    {
+                        e.target.style.backgroundColor = "green"
+                        answersChecked = true;
+                        rightanswercounter++;
+                    }
+                    else
+                    {
+                        e.target.style.backgroundColor = "red"
+                        answersChecked = true;
+                    }
+                }
+                else
+                {
+                    answersChecked = true;
+                }
+            }  
         })
-    }
+        
+    });
+    document.querySelector(".nextButton").querySelector("button").addEventListener('click',()=>{
+
+        if(counter < Questions.length-1)
+        {
+            if(answersChecked)
+            {
+            counter++;
+            answersChecked = false;
+            DeleteItems();
+            onCreate()
+            }
+        }
+        else
+        {
+            DeleteItems();
+            FinishWindow();  
+            document.querySelector(".againButton").querySelector("button").addEventListener('click',() =>{
+            counter = 0;
+            answersChecked = false;
+            rightanswercounter=0;
+            DeleteItems();
+            onCreate();
+            })
+        }
+    })  
 }
 
 
