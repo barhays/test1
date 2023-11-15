11a549912e16e5cdeb8ebd9a1a7ba91e1ef90366
vlad <lukyanovvlad2005@gmail.com>
2023-11-15 17:05:51 +0300

    fix(app.js)
    Work with localstorage
    Remove the topic

--- app.js
+++ app.js
@@ -1,53 +1,10 @@
-const question1 = {
-    numberOfQuestion: 1,
-    textOfQuestion: "How many planets are in the solar system?",
-    answers: [8,9,10,12],
-    rightanswer: [8]
-}
-const question2 = {
-    numberOfQuestion: 2,
-    textOfQuestion: "What is the freezing point of water?",
-    answers: [0,-5,-6,1],
-    rightanswer: [0]
-}
-const question3 = {
-    numberOfQuestion: 3,
-    textOfQuestion: "What is the longest river in the world?",
-    answers: ["Nile", "Amazon", "Yangtze"],
-    rightanswer: ["Nile"]
-    
-}
-const question4 = {
-    numberOfQuestion: 4,
-    textOfQuestion: "How many chromosomes are in the human genome?",
-    answers: [42, 44, 46, 58, 72],
-    rightanswer: [46]
-}
-const question5 = {
-    numberOfQuestion: 5,
-    textOfQuestion: "Which of these characters are friends with Harry Potter?",
-    answers: ["Ron Weasley", "Draco Malfoy", "Hermione Granger"],
-    rightanswer: ["Ron Weasley", "Hermione Granger"]
-}
-const question6 = {
-    numberOfQuestion: 6,
-    textOfQuestion: "What is the capital of Canada?",
-    answers: ["Toronto", "Ottawa", "Vancouver"],
-    rightanswer: ["Ottawa"]
-}
-const question7 = {
-    numberOfQuestion: 7,
-    textOfQuestion: "What is the Jewish New Year called?",
-    answers: ["Hanukkah", "Yom Kippur", "Kwanzaa", "Rosh Hashanah"],
-    rightanswer: ["Rosh Hashanah"]
-}
 
-const questions = [question1 , question2 , question3 , question4 , question5 , question6 , question7]
+import { themes } from "./topics.js";
+const questions = themes[localStorage.getItem('current_topic')];
 const wronganswercolor ="red";
 const correctanswercolor = "green";
-let counter = 0;
 let answerschecked = false;
-let rightanswercount = 0;
+let counter = localStorage.getItem('counter')
 const title = document.querySelector(".title");
 const question = document.querySelector(".question");
 const buttons = document.querySelector(".buttons");
@@ -58,20 +15,20 @@ const againButton = document.querySelector(".againButton");
 function FormCreate(questions) {
 
     let titleText = document.createElement('h3');
-    titleText.innerText = `Question ${questions[counter].numberOfQuestion}/${questions.length}`;
+    titleText.innerText = `Question ${questions[localStorage.getItem('counter')].numberOfQuestion}/${questions.length}`;
     title.appendChild(titleText);
 
     let questionText = document.createElement('h3');
-    questionText.innerText = questions[counter].textOfQuestion;
+    questionText.innerText = questions[localStorage.getItem('counter')].textOfQuestion;
     question.appendChild(questionText);
 
-    for(let i = 0; i < questions[counter].answers.length; i++) {
+    for(let i = 0; i < questions[localStorage.getItem('counter')].answers.length; i++) {
         let button = document.createElement('button');
-        button.innerText = questions[counter].answers[i];
+        button.innerText = questions[localStorage.getItem('counter')].answers[i];
         buttons.appendChild(button);
     }
 
-    if(questions[counter].rightanswer.length>1) {
+    if(questions[localStorage.getItem('counter')].rightanswer.length>1) {
         let moreInfotext = document.createElement('h3');
         moreInfotext.innerText = "This question have more then one answer";
         moreInfo.appendChild(moreInfotext);
@@ -83,6 +40,8 @@ function FormCreate(questions) {
 }
 
 function DeleteItems() {
+    let selectTopicButton = document.querySelector(".selectTopicButton")
+    selectTopicButton.innerHTML = '';
     againButton.innerHTML = '';
     nextButton.innerHTML = '';
     title.innerHTML = '';
@@ -97,37 +56,50 @@ function FinishWindow() {
     title.appendChild(titleText);
 
     let MoreInfoText = document.createElement('h3');
-    MoreInfoText.innerText = "Number of correct answers:"+ Math.round(rightanswercount);
+    MoreInfoText.innerText = "Number of correct answers:"+ localStorage.getItem('rightanswercount');
     moreInfo.appendChild(MoreInfoText);
 
     let button = document.createElement('button');
     button.innerText = "Take the test again";
     againButton.appendChild(button);
+
+    let selectTopicButton = document.querySelector(".selectTopicButton")
+    let selectbutton = document.createElement('button');
+    selectbutton.innerText = "Go back to select topic";
+    selectTopicButton.appendChild(selectbutton);
 }
 
 function AgainButtonClick() {
     againButton.querySelector("button").addEventListener('click',() => {
         counter = 0;
         answerschecked = false;
-        rightanswercount=0;
+        localStorage.setItem('counter', 0);
+        localStorage.setItem('rightanswercount', 0);
         DeleteItems();
         onCreate();
     })
 }
+function SelectTopic(){
+    document.querySelector(".selectTopicButton").querySelector("button").addEventListener('click',() => {
+        localStorage.removeItem("current_topic")
+        window.location.href='homePage.html';
+    })
+}
 function AnswersButtonClick() {
     let answers = new Set();
+    let rightanswercount = localStorage.getItem('rightanswercount')
     buttons.querySelectorAll("button").forEach(elem => {
     elem.addEventListener('click', (e)=> {
             if(answerschecked == false) {
 
-                if(questions[counter].rightanswer.length == 1) {
+                if(questions[localStorage.getItem('counter')].rightanswer.length == 1) {
 
-                    if(questions[counter].rightanswer[0] == e.target.innerText) {
+                    if(questions[localStorage.getItem('counter')].rightanswer[0] == e.target.innerText) {
 
                         e.target.style.backgroundColor = correctanswercolor;
                         answerschecked = true;
-                        rightanswercount += 1/questions[counter].rightanswer.length
-
+                        rightanswercount++;
+                        localStorage.setItem('rightanswercount', rightanswercount)
                     }else {
 
                         e.target.style.backgroundColor = wronganswercolor;
@@ -135,22 +107,23 @@ function AnswersButtonClick() {
                     }
                 }else {
 
-                    if(answers.size !== questions[counter].rightanswer.length) {
+                    if(answers.size !== questions[localStorage.getItem('counter')].rightanswer.length) {
 
                         answers.add(e.target.innerText);
 
-                        if(questions[counter].rightanswer.includes(e.target.innerText)) {
+                        if(questions[localStorage.getItem('counter')].rightanswer.includes(e.target.innerText)) {
 
                             e.target.style.backgroundColor = correctanswercolor;
-                            rightanswercount += 1/questions[counter].rightanswer.length;
-
+                            rightanswercount = +rightanswercount + 1/questions[localStorage.getItem('counter')].rightanswer.length;
+                            console.log(rightanswercount)
+                            console.log( 1/questions[localStorage.getItem('counter')].rightanswer.length)
                         }else {
 
                             e.target.style.backgroundColor = wronganswercolor;
 
                         } 
-                        if(answers.size == questions[counter].rightanswer.length) {
-
+                        if(answers.size == questions[localStorage.getItem('counter')].rightanswer.length) {
+                            localStorage.setItem('rightanswercount', Math.round(rightanswercount))
                             answerschecked=true;
 
                         }
@@ -169,6 +142,7 @@ function NextButtonClick() {
             if(answerschecked) {
 
                 counter++;
+                localStorage.setItem('counter', counter)
                 answerschecked = false;
                 DeleteItems();
 
@@ -177,6 +151,7 @@ function NextButtonClick() {
                     DeleteItems();
                     FinishWindow();  
                     AgainButtonClick();
+                    SelectTopic();
 
                 }else {
 
