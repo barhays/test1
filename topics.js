b5dc3bd579f381c5b7fd8c80723c8a9616805b65
vlad <lukyanovvlad2005@gmail.com>
2023-11-15 17:02:37 +0300

    feat:
    add new js file with topics

---
+++ topics.js
@@ -0,0 +1,83 @@
+export const Planets=[
+    {
+        numberOfQuestion: 1,
+        textOfQuestion: "How many planets are in the solar system?",
+        answers: [8,9,10,12],
+        rightanswer: [8]
+    },
+    {
+        numberOfQuestion: 2,
+        textOfQuestion: "The solar system is part of",
+        answers: ["Milky Way Galaxy", "Andromeda Galaxy"],
+        rightanswer: ["Milky Way Galaxy"]
+    },
+    {
+        numberOfQuestion: 3,
+        textOfQuestion: "The discoverer of the laws of motion of the planets of the solar system was",
+        answers: ["Nicolaus Copernicus", "Johannes Kepler", "Giordano Bruno", "Jacques Cassini"],
+        rightanswer: ["Johannes Kepler"]
+        
+    },
+    {
+        numberOfQuestion: 4,
+        textOfQuestion: "Which planets have rings?",
+        answers: ["Saturn", "Uranus", "Neptune", "Venus", "Mercury"],
+        rightanswer: ["Saturn", "Uranus"]
+    },
+    {
+        numberOfQuestion: 5,
+        textOfQuestion: "Which planet is often referred to as the Red Planet?",
+        answers: ["Mars", "Venus", "Mercury", "Jupiter"],
+        rightanswer: ["Mars"]
+    },
+    {
+        numberOfQuestion: 6,
+        textOfQuestion: "Which planet is closest to the Sun?",
+        answers: ["Mercury", "Venus", "Jupiter"],
+        rightanswer: ["Mercury"]
+    }]
+    const Sport=[
+        {
+            numberOfQuestion: 1,
+            textOfQuestion: "Which sport is known as The Beautiful Game?",
+            answers: ["Basketball", "Soccer", "Tennis","Golf"],
+            rightanswer: ["Soccer"]
+        },
+        {
+            numberOfQuestion: 2,
+            textOfQuestion: "Which country won the FIFA World Cup in 2018?",
+            answers: ["Germany", "Argentina", "France", "Brazil"],
+            rightanswer: ["France"]
+        },
+        {
+            numberOfQuestion: 3,
+            textOfQuestion: "Which of the following sports is NOT played with a ball?",
+            answers: ["Basketball", "Volleyball", "Swimming", "Tennis"],
+            rightanswer: ["Swimming"]
+            
+        },
+        {
+            numberOfQuestion: 4,
+            textOfQuestion: "Which sports are part of the triathlon?",
+            answers: ["Running", "Cycling", "Swimming", "Rowing"],
+            rightanswer: ["Running", "Cycling", "Swimming"]
+        },
+        {
+            numberOfQuestion: 5,
+            textOfQuestion: "Which athlete has won the most Olympic gold medals in history?",
+            answers: ["Usain Bolt", "Simone Biles", "Michael Phelps", "Serena Williams"],
+            rightanswer: ["Michael Phelps"]
+        },
+        {
+            numberOfQuestion: 6,
+            textOfQuestion: "Which country has won the most Olympic gold medals in total?",
+            answers: ["United States", "China", "Russia", "Germany"],
+            rightanswer: ["United States"]
+        },
+        {
+            numberOfQuestion: 7,
+            textOfQuestion: "Which of the following sports is an Olympic event?",
+            answers: ["Badminton", "Curling", "Squash", "Skateboarding", "Archery"],
+            rightanswer: ["Badminton"]
+        }]
+    export const themes=[Planets, Sport]
